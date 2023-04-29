a45223c1036b27c1fbcf3001508a396fa526117c
Ábel Bakk <abelbakk@gmail.com>
2023-04-29 15:51:51 +0200

    generated components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'museum-ticket-shop-angular';
+  currentPage = 'exhibitions';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ExhibitionsComponent } from './pages/exhibitions/exhibitions.component';
+import { TicketsComponent } from './pages/tickets/tickets.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    ExhibitionsComponent,
+    TicketsComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
