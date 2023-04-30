0d75be2f8db7b624267d3874dae814e91b58c9a1
Ábel Bakk <abelbakk@gmail.com>
2023-05-01 00:17:53 +0200

    further services

---
+++ src/app/shared/models/Exhibition.ts
@@ -0,0 +1,8 @@
+export interface Exhibition {
+  id: string;
+  name: string;
+  openFrom: string;
+  description: string;
+  descriptionLength: number;
+  imageResource: string;
+}
---
+++ src/app/shared/models/Ticket.ts
@@ -0,0 +1,8 @@
+export interface Ticket {
+  id: string;
+  price: number;
+  ticketType: string;
+  date: string;
+  userEmail: string;
+  exhibitionId: string;
+}
